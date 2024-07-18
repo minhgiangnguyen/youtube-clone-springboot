3a1001dd63e392195554f9b2e07f4a1c48d343e0
minhgiangnguyen <minhgiang1295@gmail.com>
2024-07-18 22:20:49 +0700

    -Add loading page in Home page

---
+++ src/main/resources/static/assets_home/home.js
@@ -0,0 +1,52 @@
+var width = $(window).width();
+if(width >= 1200)
+    $("#collapseSidebar").addClass("show");
+else
+    $("#collapseSidebar").removeClass("show");
+$(window).resize(function() {
+var widthResize = $(window).width();
+  if(widthResize >= 1200){
+    $("#collapseSidebar").addClass("show");
+  }else{
+    $("#collapseSidebar").removeClass("show");
+  }
+});
+
+
+const loaderContent= '<div id="loaderContent" class="loaderContent mt-3 d-flex justify-content-center">'
+                        +'<div class="loader"></div>'
+                         +'</div>';
+const textContent = '<div id="myDiv" >'
+                      +'<h2>Tadaaaaaaaaaaaaaaaaaaaaa!</h2>'
+                      +'<p>Some text in my newly loaded page..</p>'
+                    +'</div>'
+
+let pageNum = 1;
+var isAtBottom = false; // Flag to prevent multiple triggers
+
+$(window).on('scroll', function() {
+//console.log(pageNum);
+//console.log($(window).scrollTop() + $(window).height() >= $(document).height())
+     if (pageNum<totalPage && !isAtBottom
+     && $(window).scrollTop() + $(window).height() >= $(document).height()) {
+        console.log('Scrollbar is at the bottom');
+        isAtBottom = true;
+         $( "#allVideo" ).after( loaderContent);
+
+         setTimeout(() => {
+              $("#loaderContent").remove();
+              isPageVideos = true;
+
+                 pageNum++;
+                 $.post(baseUrl+"/page/"+pageNum, function(data){
+                         $('#allVideo .card-deck').append(data);
+                 })
+              isAtBottom = false; // Reset the flag
+         }, 3000);
+     }
+})
+
+
+
+
+
