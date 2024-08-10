8bc7d7d87d84bca104a2194eed90ad70d1c89628
MinhGiang <minhgiang1295@gmail.com>
2024-08-10 17:34:19 +0700

    Update MvcConfig.java

--- src/main/java/com/petproject/youtubeclone/config/MvcConfig.java
+++ src/main/java/com/petproject/youtubeclone/config/MvcConfig.java
@@ -23,6 +23,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
         if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
 
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:///"+ uploadPath + "/");
     }
 }
