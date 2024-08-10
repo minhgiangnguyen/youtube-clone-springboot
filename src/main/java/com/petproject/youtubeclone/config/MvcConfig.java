1f3f13d5c79fb3042b9d01ef27d779add16b8328
minhgiangnguyen <minhgiang1295@gmail.com>
2024-08-10 18:26:16 +0700

    -Remove folder contain file upload of user

--- src/main/java/com/petproject/youtubeclone/config/MvcConfig.java
+++ src/main/java/com/petproject/youtubeclone/config/MvcConfig.java
@@ -7,22 +7,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        exposeDirectory("user-photos", registry);
-        exposeDirectory("user-videos", registry);
-    }
-
-    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-        Path uploadDir = Paths.get(dirName);
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-
-        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
-    }
-}
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        exposeDirectory("user-photos", registry);
+//        exposeDirectory("user-videos", registry);
+//    }
+//
+//    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+//        Path uploadDir = Paths.get(dirName);
+//        String uploadPath = uploadDir.toFile().getAbsolutePath();
+//
+//        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+//
+//        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:///"+ uploadPath + "/");
+//    }
+//}
